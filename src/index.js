5f28cae78225054af89a9b5bc125d62cf0a9bf72
aberm <abermendel@gmail.com>
2019-03-19 10:27:03 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import productsReducer from './reducers/productsReducer';
+import cartReducer from './reducers/cartReducer';
+import orderReducer from './reducers/orderReducer';
+import userReducer from './reducers/userReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cartItems: cartReducer,
+  order: orderReducer,
+  user: userReducer
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+   applyMiddleware(thunk)
+ ));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
+
 serviceWorker.unregister();
